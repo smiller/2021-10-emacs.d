8bdf1f6b91f930df50d2b0e32330919b2f2b701f
Sean Miller <sean@seanmiller.ca>
2022-01-11 13:55:40 -0500

    more custom-init

--- personal/preload/custom-init.el
+++ personal/preload/custom-init.el
@@ -22,19 +22,29 @@
 
 ;; https://thewanderingcoder.com/2015/03/automating-boilerplate-in-org-mode-journalling/
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Time-Parsing.html
+(defun my/insert-line ()
+  "insert line"
+  (interactive)
+  (end-of-line)
+  (newline)
+  )
+
 (defun my/date-header ()
   "insert date-header"
-  (insert "### " (format-time-string "%A %e %B %Y")  " ###\n\n")
+  (my/insert-line)
+  (insert "### " (format-time-string "%A %-e %B %Y")  " ###\n\n")
   )
 
 (defun my/timestamp ()
   "insert timestamp"
-  (insert "" (format-time-string "%k.%M")  " ~ ")
+  (my/insert-line)
+  (insert "" (format-time-string "%-k.%M")  " ~ ")
   )
 
 (defun my/date-and-time ()
   "insert date-header and timestamp"
   (my/date-header)
+  (forward-line -1)
   (my/timestamp)
   )
 
@@ -57,26 +67,36 @@
  (kbd "C-c q")
  (defhydra hydra-quotatons (:color blue)
    "quotations"
+   ("do" (insert "°") "°")
    ("j" (insert "«") "«")
    ("k" (insert "»") "»")
-   ("u" (insert "„") "„")
-   ("i" (insert "“") "“")
+   ("go" (insert "„") "„")
+   ("gc" (insert "“") "“")
    ("ms" (insert "♯") "♯")
    ("mf" (insert "♭") "♭")
    ("mn" (insert "♮") "♮")
+   ("dh" (insert "ð") "ð")
    ("dm" (insert " — ") " — ")
    ("ns" (insert "'") "nonsmart '")
    ("nd" (insert "\"") "nonsmart \"")
    ("." (insert "…") "…")
-   ("o" (insert "°") "°")
    ("y" (insert "ʓ") "ʓ")
+   ("ea" (insert "é") "é")
+   ("Ea" (insert "É") "É")
+   ("eg" (insert "è") "è")
+   ("em" (insert "ē") "ē")
    ("-" (insert "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") "- - - line")
+   ("fb" (find-file "~/Dropbox/m/books.md") "books")
    ("fe" (find-file "~/.emacs.d/personal/preload/custom-init.el") "emacs")
    ("fo" (find-file "~/2021.emacs.d/init.el") "old-emacs")
-   ("fh" (find-file "~/Dropbox/m/gesta/2021/2021.md") "hodie")
+   ("fh" (find-file "~/Dropbox/m/gesta/2022/2022.md") "hodie")
    ("fj" (find-file "~/Dropbox/m/gesta/2020/japanese.md") "japanese")
    ("fs" (find-file "~/Dropbox/m/seans_books.md") "seans_books")
    ("fz" (find-file "~/.zshrc") ".zshrc")
+   ("ig" (insert "ì") "ì")
+   ("oe" (insert "œ") "œ")
+   ("th" (insert "þ") "þ")
+   ("ug" (insert "ù") "ù")
    ))
 
 (add-hook 'text-mode-hook 'turn-on-smart-quotes)
